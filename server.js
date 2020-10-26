dd7df6fcaace58f67ba4e5fe1c38e2eb020df9a6
mptmadhushan <tharindumadhushan420@outlook.com>
2020-10-26 13:32:40 +0530

    added fixes

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
+const cors = require("cors");
+var corsOptions = {
+  // origin: "http://localhost:3200",
+  origin: "*",
+};
+app.use(cors(corsOptions));
 
 const app = express();
 
