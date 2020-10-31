5ebd1578db865f84fe116f70a3c380f5afc7ce25
mptmadhushan <tharindumadhushan420@outlook.com>
2020-11-01 02:03:39 +0530

    added fixes

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 
+var history = require('connect-history-api-fallback');
+app.use(history());
+
 const app = express();
 
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
