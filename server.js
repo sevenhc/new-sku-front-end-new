b2c6669c193d7e7b0064ce6c5be08995914e3ca3
Hasitha Chamikara <hasitha.chamikara@sevenhc.net>
2020-11-18 23:16:54 +0530

    log port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
 
-var history = require('connect-history-api-fallback');
+var history = require("connect-history-api-fallback");
 
 const app = express();
 app.use(history());
@@ -10,4 +10,5 @@ app.use(history());
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
 const port = process.env.PORT || 8080;
+console.log(port);
 app.listen(port);
