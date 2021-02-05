6168ff5d7eb3c3c31ad7c22b8512dc33844200f6
mptmadhushan <tharindumadhushan420@outlook.com>
2021-02-06 00:17:07 +0530

    added cookies popup

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,11 @@
       <v-card class="flex" flat tile color="#2c547c">
         <v-card-title>
           <!-- <strong class="display-1">NewSku</strong> -->
-          <img :src="require('../assets/newskuwhite.png')" height="40" class="mr-3" />
+          <img
+            :src="require('../assets/newskuwhite.png')"
+            height="40"
+            class="mr-3"
+          />
           <!-- <div class="pa-3 ml-12">
             <v-col>
              <v-row>
@@ -16,12 +20,13 @@
             </v-col>
           </div>-->
           <v-spacer></v-spacer>
-          <v-btn text color="white" small to="/PrivacyPolicy">Privacy Policy</v-btn>
+          <v-btn text color="white" small to="/PrivacyPolicy"
+            >Privacy Policy</v-btn
+          >
           <v-btn text color="white" small to="/TermsOfUse">Terms of Use</v-btn>
           <!-- <v-btn text color="white" small to="/TermsAndConditions">Terms &amp; Conditions</v-btn> -->
           <v-btn text color="white" small to="/Cookies">Cookie Policy</v-btn>
-          
-          
+
           <v-spacer></v-spacer>
           <v-btn
             text="asd"
@@ -53,7 +58,7 @@
           </v-btn> -->
           <v-btn
             class="mx-1 pa-0"
-            dark           
+            dark
             href="https://www.facebook.com/NewSkufoodanddrink/"
             target="_blank"
             outlined
@@ -68,16 +73,15 @@
               width="25px"
               height="25px"
             >
-            <path
-                    d="M288,176v-64c0-17.664,14.336-32,32-32h32V0h-64c-53.024,0-96,42.976-96,96v80h-64v80h64v256h96V256h64l32-80H288z"
-                  />
-                            
+              <path
+                d="M288,176v-64c0-17.664,14.336-32,32-32h32V0h-64c-53.024,0-96,42.976-96,96v80h-64v80h64v256h96V256h64l32-80H288z"
+              />
             </svg>
           </v-btn>
 
           <v-btn
-           class="mx-1 pa-0"
-            dark           
+            class="mx-1 pa-0"
+            dark
             href="https://www.linkedin.com/company/new-sku"
             target="_blank"
             outlined
@@ -92,7 +96,13 @@
               width="25px"
               height="25px"
             >
-              <path d="m23.994 24v-.001h.006v-8.802c0-4.306-.927-7.623-5.961-7.623-2.42 0-4.044 1.328-4.707 2.587h-.07v-2.185h-4.773v16.023h4.97v-7.934c0-2.089.396-4.109 2.983-4.109 2.549 0 2.587 2.384 2.587 4.243v7.801z"/><path d="m.396 7.977h4.976v16.023h-4.976z"/><path d="m2.882 0c-1.591 0-2.882 1.291-2.882 2.882s1.291 2.909 2.882 2.909 2.882-1.318 2.882-2.909c-.001-1.591-1.292-2.882-2.882-2.882z"/>
+              <path
+                d="m23.994 24v-.001h.006v-8.802c0-4.306-.927-7.623-5.961-7.623-2.42 0-4.044 1.328-4.707 2.587h-.07v-2.185h-4.773v16.023h4.97v-7.934c0-2.089.396-4.109 2.983-4.109 2.549 0 2.587 2.384 2.587 4.243v7.801z"
+              />
+              <path d="m.396 7.977h4.976v16.023h-4.976z" />
+              <path
+                d="m2.882 0c-1.591 0-2.882 1.291-2.882 2.882s1.291 2.909 2.882 2.909 2.882-1.318 2.882-2.909c-.001-1.591-1.292-2.882-2.882-2.882z"
+              />
             </svg>
           </v-btn>
         </v-card-title>
@@ -119,11 +129,14 @@
         </v-card-text>
       </v-card>
     </v-flex>
+    <cookie-law theme="dark-lime"></cookie-law>
   </v-footer>
 </template>
 
 <script>
+import CookieLaw from "vue-cookie-law";
 export default {
+  components: { CookieLaw },
   data: () => ({
     icons: [
       { iconimg: "../assets/linkedin.svg", link: "" },
