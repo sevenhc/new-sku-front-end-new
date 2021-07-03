2e387ff8da36fd6119a99f97e8bfa4128f9602cb
SEVENHC <hasitha.chamikara@sevenhc.net>
2021-07-03 14:32:01 +0530

    Disable Trial Account

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -4,17 +4,18 @@
       <Hero />
     </div>
     <Category />
-    <Subscription/>
+    <!-- <Subscription/> -->
   </div>
 </template>
 
 <script>
 import Hero from "../components/Hero";
 import Category from "../components/Category";
-import Subscription from "../components/Subscription";
+// import Subscription from "../components/Subscription";
 export default {
   components: {
-    Hero,Subscription,
+    Hero,
+    // Subscription,
     Category,
   },
 };
